cd09372da60670a0bcecad49b4904cec429c2194
Yulya Vasilyeva <043@tut.by>
2019-10-14 21:09:21 +0300

    feat: create guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
     }
 
-    guess() {
+    setMiddle() {
+        this.middle = Math.round((this.min + this.max) / 2); 
+    }
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
+        this.min = this.middle;
+    }
 
+    guess() {
+        this.mid = this.setMiddle();
+        return this.middle;
     }
 }
-
 module.exports = GuessingGame;
